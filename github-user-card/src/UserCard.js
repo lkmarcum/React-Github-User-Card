ebbbc8d9455a0eb50b8682a1f2e619102e1ada64
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-06 17:39:32 -0500

    style adjustments

--- github-user-card/src/UserCard.js
+++ github-user-card/src/UserCard.js
@@ -2,14 +2,16 @@ import React from "react";
 
 export default function UserCard(props) {
   return (
-    <div className="card-container">
-      <img width="200" src={props.user.avatar_url} />
-      <div className="text-container">
-        <h2>{props.user.name}</h2>
-        <h4>{props.user.login}</h4>
-        <h4>{props.user.location}</h4>
-        <p>{props.user.bio}</p>
+    <a href={props.user.html_url} target="_blank">
+      <div className="card-container">
+        <img width="200" src={props.user.avatar_url} />
+        <div className="text-container">
+          <h2>{props.user.name}</h2>
+          <h4>{props.user.login}</h4>
+          <h4>{props.user.location}</h4>
+          <p>{props.user.bio}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
